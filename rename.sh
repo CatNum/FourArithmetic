1c85f7c5cd1edf95910d33faf0a74b1bbdc72c58
anini <yxl19991104@gmail.com>
2024-11-23 15:34:41 +0800

    feat:增加修改 commit 的脚本

---
+++ rename.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+## 1. 执行rename.sh （windows 下可以在 git bash 里面执行）
+## 2. 通过 git log 查看是否已修改完毕
+## 3. 同步远程仓库 git push origin --force --all
+
+git filter-branch -f --env-filter '
+
+#OLD_EMAIL=""
+OLD_NAME="anini"
+CORRECT_NAME="CatNum"
+CORRECT_EMAIL="yxl19991104@gmail.com"
+
+#if [ "$GIT_COMMITTER_EMAIL" == "$CORRECT_NAME" ] || [ "$GIT_COMMITTER_NAME" == "$CORRECT_NAME" ]
+#then
+#    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+#    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+#fi
+
+if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_EMAIL" ] || [ "$GIT_AUTHOR_NAME" != "$CORRECT_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$OLD_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+
+' -- --branches --tags
